5caa3994f4b8ee655f6b33c3d5978f069f83b75c
varashus <115972590+varashus@users.noreply.github.com>
2023-11-21 11:36:06 +0100

    update#2

---
+++ Assets/scripts/flysc.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flysc : MonoBehaviour
+{
+
+    [SerializeField] private float _velocity = 1.5f;
+    [SerializeField] private float _roataionspeed = 10f;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+     
+        
+    }
+    private void FixedUpdate()
+    {
+
+    }
+}
